5668e6bff7d4ffa36d8bf488a675e54d711547cd
Your Name <you@exmple.com>
2024-07-13 12:05:50 +0200

    add: move window command

--- lua/nvim-tmux-navigation/tmux_util.lua
+++ lua/nvim-tmux-navigation/tmux_util.lua
@@ -41,3 +41,51 @@ function util.capitalize(str)
 end
 
 return util
+
+-- -- A. Split pane, then open a new buffer in the new pane
+-- local session = "move"
+-- local window = "${session}:1"
+-- local pane = "${window}.1"
+-- -- 1. select-pane -R
+-- vim.fn.system("tmux select-pane -R")
+-- -- 2. :tabe
+-- -- vim.fn.system("tmux send-keys -t " .. "'" .. pane .. "'" .. " C-z 'n .' Enter")
+-- -- vim.fn.system("tmux send-keys -t " .. "'" .. pane .. "'" .. " 'n .' Enter")
+-- vim.fn.system("tmux send-keys 'nvim " .. name .. "' Enter")
+
+-- local number_of_tmux_panes = vim.fn.system("tmux list-panes | wc -l")
+-- print("number_of_tmux_panes: ", vim.inspect(number_of_tmux_panes))
+--
+-- -- B. To Move B1 to the right
+-- local opposite_directions = { L = "R", R = "L", U = "D", D = "U" }
+-- local direction = "R"
+-- local opposite_direction = opposite_directions[direction]
+-- vim.fn.system("tmux select-pane -" .. direction) -- 1. go to the right pane
+-- local isNvim = vim.fn.has("nvim") -- TODO: does not work, maybe have to add two separate commands?
+-- print("isNvim: ", vim.inspect(isNvim))
+-- if isNvim then
+-- 	vim.fn.system("tmux send-keys ':tabe' Enter") -- 2.2.1 open new tab
+-- 	vim.fn.system("tmux send-keys ':e " .. name .. "'" .. " Enter") -- 2.2.2 if vim open
+-- else
+-- 	vim.fn.system("tmux send-keys 'nvim " .. name .. "' Enter") -- 2.1 if only tmux
+-- end
+-- vim.fn.system("tmux select-pane -" .. opposite_direction) -- 3. go back (to clean up)
+--
+-- local function get_number_of_tabs()
+-- 	local number_of_tabs = 0
+-- 	for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
+-- 		number_of_tabs = number_of_tabs + 1
+-- 	end
+-- 	return number_of_tabs
+-- end
+-- local number_of_tabs = get_number_of_tabs()
+-- -- local number_of_tabs = 0
+-- if number_of_tabs == 1 then
+-- 	print("tmux kill")
+-- 	vim.fn.system("tmux kill-pane") -- 4.1
+-- elseif number_of_tabs > 1 then
+-- 	print("vim q")
+-- 	pcall(vim.cmd, "q") -- 4.2
+-- end
+-- vim.fn.system("tmux send-keys Enter") -- 5. Enter to not have "Press Enter or type command to continue"
+
